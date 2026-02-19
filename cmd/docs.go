427c9576955c225cfeafbefb5a88ca1598af48fd
Erin Atkinson <20285458+erindatkinson@users.noreply.github.com>
2026-02-18 17:01:00 -0700

    update cmd for docgen
    Signed-off-by: Erin Atkinson <20285458+erindatkinson@users.noreply.github.com>

--- cmd/docs.go
+++ cmd/docs.go
@@ -1,9 +1,9 @@
 package cmd
 
 import (
-	"fmt"
-	"slices"
-	"strings"
+	"os"
+	"path"
+	"text/template"
 
 	"github.com/erindatkinson/slack-emojinator/internal/templates"
 	"github.com/erindatkinson/slack-emojinator/internal/utilities"
@@ -44,57 +44,49 @@ var docsCmd = &cobra.Command{
 		inputRoot := cmd.Flag("input-root").Value.String()
 
 		logger.Info("getting emoji list")
-		emojis, err := templates.ListEmojiFiles(inputRoot)
+		emojis, err := templates.ListEmojiFiles(inputRoot, namespace)
 		if err != nil {
 			logger.Error("unable to get emojis", "error", err)
 			return
 		}
 
-		logger.Info("loading template")
-		tpl, err := templates.LoadTemplate("templates/doc_page.md.jinja2")
+		indexTpl, err := template.New("index").Parse(string(templates.MustAsset("templates/doc_index.md.gotmpl")))
 		if err != nil {
-			logger.Error("unable to load stored template", "error", err)
+			logger.Error("error loading template", "error", err)
+			return
+		}
+
+		tpl, err := template.New("docs").Parse(string(templates.MustAsset("templates/doc_page.md.gotmpl")))
+		if err != nil {
+			logger.Error("error loading template", "error", err)
 			return
 		}
 
 		logger.Info("paginating emojis")
-		pages := templates.PaginateEmojiList(emojis)
-		keys := make([]string, 0, len(pages))
-		for k := range pages {
-			keys = append(keys, k)
+		pages := templates.PaginateEmojiList(emojis, namespace)
+		doc := templates.Docs{Namespace: namespace, Pages: pages}
+		os.MkdirAll(path.Join("docs/", namespace), 0700)
+		indexFp, err := os.Create(path.Join("docs/", namespace, "index.md"))
+		if err = indexTpl.Execute(indexFp, &doc); err != nil {
+			logger.Error("error writing index", "error", err)
+			indexFp.Close()
+			return
 		}
+		indexFp.Close()
 
-		logger.Info("sorting keys", "pages", len(keys))
-		slices.Sort(keys)
-
-		logger.Info("building template data")
-		for i := 0; i < len(keys); i++ {
-			data := make(map[string]any)
-			key := keys[i]
-			keySplits := strings.Split(key, "-")
-			data["page_count"] = keySplits[2]
-			if i > 0 {
-				data["prev_page"] = keys[i-1]
-			} else if i < len(keys)-1 {
-				data["next_page"] = keys[i+1]
-			}
-
-			page := pages[key]
-			emojiData := make(map[string]string)
-			for _, emoji := range page {
-				emojiData["name"] = emoji.Name
-				emojiData["path"] = emoji.DocPath
-			}
-			data["emoji"] = emojiData
-			rendered, err := templates.RenderWithData(*tpl, data)
+		for _, page := range pages {
+			fp, err := os.Create(path.Join("docs/", namespace, page.Name+".md"))
 			if err != nil {
-				logger.Error("unable to render data", "error", err)
+				logger.Error("couldn't make file", "error", err)
 				return
 			}
-			fmt.Println(rendered)
-			if i > 0 {
+
+			if err = tpl.Execute(fp, *page); err != nil {
+				logger.Error("error writing file", "error", err)
+				fp.Close()
 				return
 			}
+			fp.Close()
 		}
 
 	},
