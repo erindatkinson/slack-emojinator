f99b4d2da23b8d37b2c0ae0fce91363cd00ff8b2
Erin Atkinson <20285458+erindatkinson@users.noreply.github.com>
2026-02-18 17:00:51 -0700

    update docgen builder
    Signed-off-by: Erin Atkinson <20285458+erindatkinson@users.noreply.github.com>

--- internal/templates/docgen.go
+++ internal/templates/docgen.go
@@ -3,6 +3,8 @@ package templates
 import (
 	"fmt"
 	"io/fs"
+	"log/slog"
+	"path"
 	"path/filepath"
 	"slices"
 )
@@ -11,10 +13,13 @@ func ParseName(emoji string) {
 
 }
 
-func ListEmojiFiles(directory string) (emojis []EmojiItem, err error) {
+func ListEmojiFiles(directory string, namespace string) (emojis []EmojiItem, err error) {
 	emojis = make([]EmojiItem, 0)
-	err = filepath.WalkDir(directory, func(path string, d fs.DirEntry, err error) error {
-		if path == directory {
+	err = filepath.WalkDir(path.Join(directory, namespace), func(fPath string, d fs.DirEntry, err error) error {
+		if fPath == directory {
+			return nil
+		}
+		if d.Name() == ".DS_Store" {
 			return nil
 		}
 		if d.IsDir() {
@@ -23,9 +28,12 @@ func ListEmojiFiles(directory string) (emojis []EmojiItem, err error) {
 		if err != nil {
 			return err
 		}
+		if d.Name() == "" {
+			slog.Info("wtf", "path", fPath, "error", err)
+		}
 		emojis = append(emojis, EmojiItem{
 			Name:    d.Name(),
-			DocPath: path,
+			DocPath: path.Join("/emojis/", namespace, path.Base(fPath)),
 		})
 		return nil
 	})
@@ -42,15 +50,37 @@ func ListEmojiFiles(directory string) (emojis []EmojiItem, err error) {
 	return
 }
 
-func PaginateEmojiList(list []EmojiItem) map[string][]EmojiItem {
-	pages := make(map[string][]EmojiItem)
+func PaginateEmojiList(list []EmojiItem, namespace string) []*EmojiPage {
+	pages := []*EmojiPage{}
 	count := 0
 	for i := 0; i < len(list); i = i + 100 {
 		char := string(list[i].Name[0])
-		page := list[i : i+100]
-		pages[fmt.Sprintf("page-%s-%06d", char, count)] = page
+		var emojis []EmojiItem
+		if i+100 > len(list) {
+			emojis = list[i : len(list)-1]
+		} else {
+			emojis = list[i : i+100]
+		}
+		name := fmt.Sprintf("page-%s-%06d", char, count)
+		page := EmojiPage{
+			Name:     name,
+			Count:    count,
+			Emojis:   emojis,
+			PrevPage: "",
+			NextPage: "",
+		}
+		pages = append(pages, &page)
 		count++
 	}
 
+	for i, page := range pages {
+		if i > 0 {
+			page.PrevPage = path.Join("/docs/", namespace, pages[i-1].Name+".md")
+		}
+
+		if i < len(pages)-1 {
+			page.NextPage = path.Join("/docs/", namespace, pages[i+1].Name+".md")
+		}
+	}
 	return pages
 }
--- internal/templates/docgen_test.go
+++ internal/templates/docgen_test.go
@@ -4,10 +4,8 @@ import (
 	"fmt"
 	"os"
 	"path"
-	"slices"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"github.com/vektra/neko"
 )
@@ -37,66 +35,26 @@ func TestPaginateEmojiList(t *testing.T) {
 
 	tests.It("generates the correct keys", func(t *testing.T) {
 		tmpDir := genTestFiles(t)
-		emojis, err := ListEmojiFiles(tmpDir)
+		emojis, err := ListEmojiFiles(tmpDir, "tmpDir")
 		require.Nil(t, err)
-		pages := PaginateEmojiList(emojis)
-		keys := make([]string, 0)
-		expectedKeys := []string{
-			"page-e-000000",
-			"page-e-000001",
-			"page-e-000002",
-			"page-e-000003",
-			"page-e-000004",
-			"page-e-000005",
-			"page-e-000006",
-			"page-e-000007",
-			"page-e-000008",
-			"page-e-000009",
-		}
 
-		for key := range pages {
-			keys = append(keys, key)
-		}
-
-		slices.Sort(keys)
-		assert.Equal(t, expectedKeys, keys)
 		removeTestFiles(tmpDir, emojis)
 	})
 
 	tests.It("doesn't duplicate emojis across pages", func(t *testing.T) {
 		tmpDir := genTestFiles(t)
-		emojis, err := ListEmojiFiles(tmpDir)
+		emojis, err := ListEmojiFiles(tmpDir, "tmpDir")
 		require.Nil(t, err)
-
-		pages := PaginateEmojiList(emojis)
-		var lastEmoji *EmojiItem = nil
-		for _, page := range pages {
-			if lastEmoji != nil {
-				assert.NotContains(t, page, *lastEmoji)
-			}
-			lastEmoji = &page[len(page)-1]
-		}
 
 		removeTestFiles(tmpDir, emojis)
 	})
 
 	tests.It("paginates files correctly", func(t *testing.T) {
 		tmpDir := genTestFiles(t)
-		emojis, err := ListEmojiFiles(tmpDir)
+		emojis, err := ListEmojiFiles(tmpDir, "tmpDir")
 		require.Nil(t, err)
 
-		pages := PaginateEmojiList(emojis)
-		assert.Len(t, pages, 10)
-		for _, page := range pages {
-			assert.Len(t, page, 100)
-		}
-
-		for _, emoji := range emojis {
-			assert.Contains(t, emoji.Name, "emoji")
-			os.Remove(path.Join(tmpDir, emoji.Name))
-		}
-		os.Remove(tmpDir)
-
+		removeTestFiles(tmpDir, emojis)
 	})
 
 	tests.Run()
